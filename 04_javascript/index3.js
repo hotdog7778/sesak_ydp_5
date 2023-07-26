cc11c9e72094dbf7fe0a012c8b72434824c11dee
김태균 <rlaxorbs159@gmail.com>
2023-07-26 14:53:04 +0900

    study: Js 문자형변환_숫자형변환

--- 04_javascript/index3.js
+++ 04_javascript/index3.js
@@ -164,9 +164,59 @@ console.log(typeof fruits);
 console.log(typeof cat);
 
 // 자신을 소개하는 object 형태의 변수를 선언
-let me = {
-  name: '김태균',
-  age: 32,
-  job: '백수',
-};
-console.log(me);
+// let me = {
+//   name: '김태균',
+//   age: 32,
+//   job: '백수',
+// };
+// console.log(me);
+
+// 형변환
+// 성적 평균 구하는 프로그램 만들기
+
+// let mathScore = prompt('수학점수 입력');
+// console.log(mathScore, typeof mathScore);
+// prompt 로 입력받은 값은 String 타입으로 반환 됩니다.
+// let engScore = prompt('영어점수 입력');
+// console.log(engScore, typeof engScore);
+
+// let avg = (mathScore + engScore) / 2;
+// 그래서 mathScore + engScore 연산은 num 타입끼리의 연산이 아닌 String 타입 끼리의 연산이 됩니다.
+// 이후 /2 를했을때 2는 num타입이며, String 타입과 num 타입의 연산이 진행됩니다. 그리고 num 타입으로 자동 형변환 됩니다.
+
+// console.log(`수학과 영어의 평균 점수는 ${avg}점 입니다.`);
+
+// 1. String(): 문자로 형변환
+let str1 = true;
+let str2 = 123;
+let str3 = null;
+
+console.log(String(str1), typeof String(str1));
+console.log(String(str2), typeof String(str2));
+console.log(String(str3), typeof String(str3));
+console.log(str1.toString(), typeof str1.toString());
+
+// 2. Number(): 숫자로 형변환
+let n1 = true;
+let n2 = false;
+let n3 = '123.9'; // string
+console.log(Number(n1), typeof Number(n1)); // ture는 number 로 형변환 되어 1 출력한다.
+console.log(Number(n2), typeof Number(n2)); // false는 number 로 형변환 되어 0 출력한다.
+console.log(Number(n3), typeof Number(n3)); // 123.9
+console.log(parseInt(n3, 10)); // 123 ** 문자열을 10진수 숫자로 변환시켜준다.
+console.log(parseFloat(n3, 10)); // 123.9 ** 소수점 까지 포함한 숫자로 변환
+
+// 형변환 실습
+let mathScore = '77';
+let engScore = '88';
+
+let numMathScore = Number(mathScore);
+let numEngScore = Number(engScore);
+
+let avgScore = (numMathScore + numEngScore) / 2;
+
+console.log(avgScore);
+
+// +) null, undefined를 숫자로 바꾼다면?
+console.log(Number(undefined), typeof Number(undefined)); // NaN(Not a Number) Number ** 숫자가 아니라는 값이 나오지만 자료형 타입은 Number
+console.log(Number(null), typeof Number(null)); // 0 Number
