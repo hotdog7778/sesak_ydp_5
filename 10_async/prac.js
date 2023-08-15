576ad24292052cdb309612320efd12c5d7b12acd
김태균 <rlaxorbs159@gmail.com>
2023-08-15 17:06:11 +0900

    studey: js async awaite 및 express get post

---
+++ 10_async/prac.js
@@ -0,0 +1,5 @@
+new Promise(function (resolve, reject) {
+  setTimeout(function (name) {
+    resolve();
+  });
+});
---
+++ 11_form/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('/views', 'views');
+
+// 미들웨어 bodyParser 등록
+app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든 형식의 데이터를 파싱
+app.use(express.json()); // json 형식으로 데이터를 주고 받음
+
+// 라우팅
+app.get('/', (req, res) => {
+  res.render('index', { title: '폼 전송을 연습!' });
+});
+
+app.get('/getForm', (req, res) => {
+  console.log(req.query);
+  //
+  res.render('result', {
+    title: 'GET 요청',
+    userInfo: req.query,
+  });
+});
+
+app.post('/postForm', (req, res) => {
+  console.log(req.body);
+  //res.send('POST 요청 성공');
+  res.render('result', {
+    title: 'POST 요청',
+    userInfo: req.body,
+  });
+});
+
+// 메인 페이지
+app.get('/prac', (req, res) => {
+  res.render('prac/index');
+});
+
+app.get('/prac/join', (req, res) => {
+  //res.send('GET 요청 성공');
+  //res.render('prac/index', info);
+  console.log(req.query);
+  res.render('prac/join', {
+    title: 'GET 요청',
+    memberInfo: req.query,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} 포트로 서버 시작`);
+});
