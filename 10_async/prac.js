a5fdb9b45bfcc56c8a729b8a254bad6d6db6830c
김태균 <rlaxorbs159@gmail.com>
2023-08-16 13:16:49 +0900

    study: 콜백지옥을 프로미스코드로 변경, 프로미스에 파라미터 사용하기

--- 10_async/prac.js
+++ 10_async/prac.js
@@ -1,5 +1,43 @@
-new Promise(function (resolve, reject) {
-  setTimeout(function (name) {
-    resolve();
+// function call(name) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log(name);
+//       resolve(name);
+//     }, 1000);
+//   });
+// }
+
+const call = (name) => {
+  new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      console.log(name);
+      resolve(name);
+    }, 1000);
   });
-});
+};
+function back() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('back');
+      resolve('back');
+    }, 1000);
+  });
+}
+function hell() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('callback hell');
+    }, 1000);
+  });
+}
+
+call('kim')
+  .then(function (result) {
+    console.log(`${result} 반가워`);
+    return back();
+  })
+  .then(function (result) {
+    console.log(`${result}를 실행했구나`);
+    return hell();
+  })
+  .then((result) => console.log(`여기는 ${result}`));
