ac8073ffc74322bb08a8fa34f8cbc5e0efeb8771
hotdog7778 <rlaxorbs159@gmail.com>
2023-09-29 08:09:31 +0900

    study: react state 학습

--- 22_react/state-app/src/components/CounterFunction.js
+++ 22_react/state-app/src/components/CounterFunction.js
@@ -2,13 +2,19 @@ import { useState } from 'react';
 
 const CounterFunction = (props) => {
   const [number, setNumber] = useState(0);
-  const onClickEnter = () => setNumber(number + 1);
+  //const onClickEnter = () => setNumber(number + 1);
   const { value } = props;
 
   return (
     <div>
       <h1>{number}</h1>
-      <button onClick={onClickEnter}>{value}</button>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        {value}
+      </button>
     </div>
   );
 };
