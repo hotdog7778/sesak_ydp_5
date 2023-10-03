8327beb0c8813b27d8eb2391931a287d6aa85dff
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-03 17:58:26 +0900

    delete unnecessary files

--- groupstudy/ref/app2.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type' : 'text/plain' });
-  res.end('Hello World');
-});
-
-server.listen(8000);
