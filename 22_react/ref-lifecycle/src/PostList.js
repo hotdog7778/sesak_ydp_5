6792da4b53178985f0f7fd68ece962f3f4c44ab6
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-09 22:00:52 +0900

    study: useEffect, axios 퀴즈

--- 22_react/ref-lifecycle/src/PostList.js
+++ 22_react/ref-lifecycle/src/PostList.js
@@ -1,16 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PostItem from './PostItem'
 
 export default function PostList() {
-  const [state, setState] = useState({
-    visible: false,
-  })
-
     return (
     <div>
         <h1>PostList</h1>
-        <PostItem />
-        
+        <PostItem />     
     </div>
   )
 }
