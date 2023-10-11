62fa2e45f3f754880dbe183447ddfb825ce332db
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-11 18:20:42 +0900

    study: TodoList - 백엔드&프론트엔드 각각 구현하기
    description :
     프론트엔드 Todo리스트 화면 출력

---
+++ 23_todo_list/client/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Todo from './components/Todo';
+
+function App() {
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 1,
+      title: 'my todo1',
+      done: false,
+    },
+    {
+      id: 2,
+      title: 'my todo2',
+      done: false,
+    },
+    {
+      id: 3,
+      title: 'my todo3',
+      done: true,
+    },
+    {
+      id: 4,
+      title: '밥먹기',
+      done: false,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      {/* todoItems 반복, props로 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
+      {todoItems.map((item) => {
+        return <Todo key={item.id} item={item} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ 23_todo_list/client/src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Todo({ item }) {
+  const { id, title, done } = item;
+
+  return (
+    <div>
+      <input type="checkbox" name={`todo${id}`} id={`todo${id}`} defaultChecked={done}></input>
+      <label htmlFor={`todo${id}`}>{title}</label>
+    </div>
+  );
+}
