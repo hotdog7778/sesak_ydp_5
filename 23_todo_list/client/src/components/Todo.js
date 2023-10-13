4edb66fa3630829ea788948ce3f594c94d832774
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-13 14:05:42 +0900

    study: TodoList - 백엔드&프론트엔드 각각 구현하기- 프론트엔드 Todo 수정하기

--- 23_todo_list/client/src/components/Todo.js
+++ 23_todo_list/client/src/components/Todo.js
@@ -3,15 +3,64 @@ import React, { useState } from 'react';
 export default function Todo({ item, deleteItem }) {
   const [todoItem, setTodoItem] = useState(item);
   const { id, title, done } = todoItem;
+  const [readOnly, setReadOnly] = useState(true);
 
   const onDeleteBtnClick = () => {
     deleteItem(todoItem);
   };
 
+  // title 클릭시 readonly를 false로 변경
+  const offReadOnlyMode = () => {
+    //setReadOnly(false);
+  };
+
+  const editEventHandler = (e) => {
+    const { title, ...rest } = todoItem;
+    console.log(rest);
+    /*
+    rest
+    {
+      id : ~~,
+      done : ~~,
+    }
+    */
+    setTodoItem({
+      title: e.target.value,
+      ...rest,
+    });
+  };
+
+  const editKey = (e) => {
+    if (e.key === 'Enter') {
+      setReadOnly(true);
+    }
+  };
+
+  // 포커스 인
+  const onFocus = (e) => {
+    console.log(e);
+    setReadOnly(false);
+  };
+
+  // 포커스 아웃
+  const onBlur = (e) => {
+    console.log(e);
+    setReadOnly(true);
+  };
+
+  const checkboxEventHandler = (e) => {
+    const { done, ...rest } = todoItem;
+    setTodoItem({
+      done: e.target.checked,
+      ...rest,
+    });
+  };
+
   return (
     <div>
-      <input type="checkbox" name={`todo${id}`} id={`todo${id}`} defaultChecked={done}></input>
-      <label htmlFor={`todo${id}`}>{title}</label>
+      <input type="checkbox" name={`todo${id}`} id={`todo${id}`} defaultChecked={done} onChange={checkboxEventHandler}></input>
+      {/* <label htmlFor={`todo${id}`}>{title}</label> */}
+      <input type="text" value={todoItem.title} readOnly={readOnly} onClick={offReadOnlyMode} onChange={editEventHandler} onKeyDown={editKey} onFocus={onFocus} onBlur={onBlur} />
       <button onClick={onDeleteBtnClick}>삭제</button>
     </div>
   );
