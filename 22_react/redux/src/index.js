5bb4cea1016c2208160c432c60ae1fdf2f7cbad4
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-18 14:25:41 +0900

    study: redux 적용

--- 22_react/redux/src/index.js
+++ 22_react/redux/src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App2 from './App2';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'; // store를 정의하는 메서드 configureStore()
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const initialState = {
+  number: 50,
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PLUS':
+      return { number: state.number + 1 };
+    case 'MINUS':
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+};
+
+// 전역 상태를 관리하는 공간 (하나의 프로젝트에 하나만)
+const store = configureStore({ reducer }, composeWithDevTools);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App2 />
+    </Provider>
   </React.StrictMode>
 );
